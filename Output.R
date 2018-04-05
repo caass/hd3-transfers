b0414725729b95c18ee2d8b07386c10e23a62ed6
Daniel Fridkin <danielfridkin@gmail.com>
2018-04-05 16:04:25 -0400

    Updated output

--- Output.R
+++ Output.R
@@ -11,5 +11,6 @@ source('Plotting.R')
 # edge_widths = c('uniform', 'transfers', 'ari') -- Edge widths uniform or proportional to log(transfers or ari)
 # highlight_facility = c(FALSE, TRUE) -- Highlight a bridging facility?
 
+par(bg = 'grey87')
 plot_network(label_clusters = FALSE, node_sizes = 'stays', node_colors = 'cases', edges_to_plot = 'all',
              edge_colors = 'percent_ari', edge_widths = 'transfers', highlight_facility = FALSE)
